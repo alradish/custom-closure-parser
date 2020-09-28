6434ef46c97f7301d0d1dda88ce4b8bcbd418308
Alexander Raish <leidenn2509@gmail.com>
2020-09-28 18:19:34 +0700

    Add better-parser dependence

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,10 @@ version = "0.0.1"
 
 repositories {
     mavenCentral()
+    maven { setUrl("https://dl.bintray.com/hotkeytlt/maven") }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("com.github.h0tk3y.betterParse:better-parse:0.4.0")
 }
