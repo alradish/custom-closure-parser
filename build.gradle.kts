eaaa2b9002ea02ffa90f1159c4283e47e691c148
Alexander Raish <leidenn2509@gmail.com>
2020-09-28 18:02:29 +0700

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "io.github.alrai"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
