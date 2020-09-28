9b657efd3d0877d4f75faf837c873d8ccb325585
Alexander Raish <leidenn2509@gmail.com>
2020-09-28 22:20:43 +0700

    Remove better-parser and use nashorn instead. Parse little example

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,8 @@ version = "0.0.1"
 
 repositories {
     mavenCentral()
-    maven { setUrl("https://dl.bintray.com/hotkeytlt/maven") }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.github.h0tk3y.betterParse:better-parse:0.4.0")
 }
