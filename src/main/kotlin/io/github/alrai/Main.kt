2b7d467ae5e3285b5c0a48a27ed09b22ce78dd56
Alexander Raish <leidenn2509@gmail.com>
2020-10-01 14:47:14 +0700

    Play with rhino

--- src/main/kotlin/io/github/alrai/Main.kt
+++ src/main/kotlin/io/github/alrai/Main.kt
@@ -1,14 +1,40 @@
 package io.github.alrai
 
 import org.mozilla.javascript.CompilerEnvirons
+import org.mozilla.javascript.IRFactory
 import org.mozilla.javascript.Parser
+import org.mozilla.javascript.Token
+import org.mozilla.javascript.optimizer.Codegen
+
 
 fun main() {
     val environment = CompilerEnvirons()
+    Token.printTrees
     val parser = Parser(environment)
     val root = parser.parse(
-        "var a = 10; var b = a + 1;" +
-                "function someFunction() { return b + 1; }  ", null, 1
+        """
+            var x = 10; var y = x + 1;
+            function someFunction() { return y + 1; }
+            function foo(a) {
+              var b = 42;
+              function bar(c) {
+                return a + b + c;
+              };
+              return bar(24);
+            };
+
+        """.trimIndent(), null, 1
     )
-    println(root.debugPrint())
+
+    println(root.toSource())
+    val irf = IRFactory(environment)
+    val tree = irf.transformTree(root)
+    println(tree.toSource())
+
+    val codegen = Codegen()
+    codegen.setMainMethodClass("someFunction")
+    codegen.compileToClassFile(
+        environment, "Main", tree, tree.encodedSource,
+        false
+    )
 }
