9fc38b30f49e272b8948d6e6e6d9d917adf46dec
Alexander Raish <leidenn2509@gmail.com>
2020-10-01 14:47:36 +0700

    Init junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,4 +12,20 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(group= "org.mozilla", name= "rhino", version="1.7.13")
+
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+// config JVM target to 1.8 for kotlin compilation tasks
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "1.8"
 }
---
+++ src/test/kotlin/io/github/alrai/VisitorTests.kt
@@ -0,0 +1,10 @@
+package io.github.alrai
+
+import org.junit.jupiter.api.Test
+
+class VisitorTests {
+    @Test
+    fun findSimpleClojure() {
+
+    }
+}
