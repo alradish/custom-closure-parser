88f3da0a722ca0bb6e50744e1fe0e16398015a1d
Alexander Raish <leidenn2509@gmail.com>
2020-10-04 13:38:08 +0700

    Refactor skip logic in UsedSymbolsVisitor.kt

--- src/main/kotlin/io/github/alrai/visitors/UsedSymbolsVisitor.kt
+++ src/main/kotlin/io/github/alrai/visitors/UsedSymbolsVisitor.kt
@@ -6,17 +6,14 @@ import org.mozilla.javascript.ast.*
 class UsedSymbolsVisitor() : NodeVisitor {
     val usedSymbols: MutableMap<AstNode, MutableList<Name>> = mutableMapOf()
     override fun visit(node: AstNode): Boolean {
-        if(node is Name) {
+        if (node is Name) {
+
+            // Skip Name in function creation and params
             val parent = node.parent
-            if (parent is FunctionNode && node == parent.functionName) {
-                return false
+            if (parent is FunctionNode) {
+                if (node == parent.functionName) return false
+                if (parent.params.contains(node)) return false
             }
-            // Delete this later, cos symbolsTable contains params symbols
-            if(parent is FunctionNode && parent.params.contains(node)) {
-                return false
-            }
-
-
 
             val key = node.ancestorWithType<Scope>()!!
             if (usedSymbols[key] == null) {
