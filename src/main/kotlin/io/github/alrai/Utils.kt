12161821d3821374c8cedebb7969f684b74acd55
Alexander Raish <leidenn2509@gmail.com>
2020-10-04 13:31:40 +0700

    Add utils haveAncestor and ancestorWithType

--- src/main/kotlin/io/github/alrai/Utils.kt
+++ src/main/kotlin/io/github/alrai/Utils.kt
@@ -2,6 +2,23 @@ package io.github.alrai
 
 import org.mozilla.javascript.ast.AstNode
 
+
+fun AstNode.haveAncestor(node: AstNode): Boolean {
+    return this.parent?.let {
+        if (it == node) true else it.haveAncestor(node)
+    } ?: false
+}
+
+inline fun <reified T : AstNode> AstNode.ancestorWithType(): AstNode? {
+    var p = parent
+    while (p != null) {
+        if (p is T) return p
+        p = p.parent
+    }
+    return null
+}
+
+
 fun unreachable(): Nothing = error("Unreachable code")
 fun unknownAstNode(node: AstNode): Nothing =
     error("Unknown ast node: ${node::class.simpleName}::${node.toSource()}")
